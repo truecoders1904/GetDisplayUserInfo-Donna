3897efffe8f3a47ea8e785df8fad0e357bb6fb68
dwebhack57 <49411089+dwebhack57@users.noreply.github.com>
2019-04-23 15:37:45 -0700

    filled Out Display

--- DisplayInfo/Program.cs
+++ DisplayInfo/Program.cs
@@ -6,7 +6,28 @@ namespace DisplayInfo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your firstname?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your lastname");
+            string lasttName = Console.ReadLine();
+            Console.WriteLine("what is your favorite number?");
+            int favoriteNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your favorite animal?");
+            string favoriteAnimal = Console.ReadLine();
+            Console.WriteLine("Any other information you want to ask about?");
+            Console.WriteLine(firstName + " " + lasttName);
+            Console.WriteLine(favoriteNumber + " " + favoriteAnimal);
+            if (favoriteNumber > 10)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(favoriteNumber);
+            }
+            else if (favoriteNumber <5)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(favoriteNumber);
+            }
         }
+
     }
 }
